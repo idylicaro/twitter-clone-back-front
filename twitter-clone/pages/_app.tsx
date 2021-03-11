42c5c1e2a254115e0c2d7a63de4664a8249b3a75
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-03-11 11:59:53 -0300

    feat: init project configs

---
+++ twitter-clone/pages/_app.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }): JSX.Element {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ twitter-clone/pages/index.tsx
@@ -0,0 +1,9 @@
+export const Home = (): JSX.Element => {
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default Home
